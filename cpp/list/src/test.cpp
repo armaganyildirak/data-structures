f25e306bd3317742f2dde5b80a56a8a8239261e6
armaganyildirak <armaganyildirak@gmail.com>
2024-09-16 20:07:10 -0400

    formatting code

--- cpp/list/src/test.cpp
+++ cpp/list/src/test.cpp
@@ -1,61 +1,61 @@
-#include <cassert>
 #include "list.h"
+#include <cassert>
 
 void test_insert_node() {
-    List<int> list;
-    int data1 = 10;
-    int data2 = 20;
-    int data3 = 30;
+  List<int> list;
+  int data1 = 10;
+  int data2 = 20;
+  int data3 = 30;
 
-    list.insert_node(data1);
-    assert(list.head->data == 10);
+  list.insert_node(data1);
+  assert(list.head->data == 10);
 
-    list.insert_node(data2);
-    assert(list.head->next->data == 20);
+  list.insert_node(data2);
+  assert(list.head->next->data == 20);
 
-    list.insert_node(data3);
-    assert(list.head->next->next->data == 30);
-    assert(list.head->next->next->next == nullptr);
+  list.insert_node(data3);
+  assert(list.head->next->next->data == 30);
+  assert(list.head->next->next->next == nullptr);
 }
 
 void test_delete_node() {
-    List<int> list;
-    int data1 = 10;
-    int data2 = 20;
-    int data3 = 30;
+  List<int> list;
+  int data1 = 10;
+  int data2 = 20;
+  int data3 = 30;
 
-    list.insert_node(data1);
-    list.insert_node(data2);
-    list.insert_node(data3);
+  list.insert_node(data1);
+  list.insert_node(data2);
+  list.insert_node(data3);
 
-    list.delete_node(data2);
-    assert(list.head->next->data == 30);
+  list.delete_node(data2);
+  assert(list.head->next->data == 30);
 
-    list.delete_node(data1);
-    assert(list.head->data == 30);
+  list.delete_node(data1);
+  assert(list.head->data == 30);
 
-    list.delete_node(data3);
-    assert(list.head == nullptr);
+  list.delete_node(data3);
+  assert(list.head == nullptr);
 }
 
 void test_print_list() {
-    List<int> list;
-    int data1 = 10;
-    int data2 = 20;
-    int data3 = 30;
+  List<int> list;
+  int data1 = 10;
+  int data2 = 20;
+  int data3 = 30;
 
-    list.insert_node(data1);
-    list.insert_node(data2);
-    list.insert_node(data3);
+  list.insert_node(data1);
+  list.insert_node(data2);
+  list.insert_node(data3);
 
-    list.print_list();
+  list.print_list();
 }
 
 int main() {
-    test_insert_node();
-    test_delete_node();
-    test_print_list();
+  test_insert_node();
+  test_delete_node();
+  test_print_list();
 
-    std::cout << "All tests passed!" << std::endl;
-    return 0;
+  std::cout << "All tests passed!" << std::endl;
+  return 0;
 }
--- cpp/stack/src/test.cpp
+++ cpp/stack/src/test.cpp
@@ -1,49 +1,48 @@
+#include "stack.h"
 #include <cassert>
 #include <iostream>
-#include "stack.h"
-
 
 void test_stack_push() {
-    Stack<int> stack;
-    stack.push(10);
-    stack.push(20);
+  Stack<int> stack;
+  stack.push(10);
+  stack.push(20);
 
-    assert(stack.peek() == 20);
+  assert(stack.peek() == 20);
 }
 
 void test_stack_pop() {
-    Stack<int> stack;
-    stack.push(10);
-    stack.push(20);
+  Stack<int> stack;
+  stack.push(10);
+  stack.push(20);
 
-    int popped = stack.pop();
-    assert(popped == 20);
-    assert(stack.peek() == 10);
+  int popped = stack.pop();
+  assert(popped == 20);
+  assert(stack.peek() == 10);
 }
 
 void test_stack_peek() {
-    Stack<int> stack;
-    stack.push(30);
-    assert(stack.peek() == 30);
+  Stack<int> stack;
+  stack.push(30);
+  assert(stack.peek() == 30);
 }
 
 void test_stack_is_empty() {
-    Stack<int> stack;
+  Stack<int> stack;
 
-    assert(stack.is_empty());
+  assert(stack.is_empty());
 
-    stack.push(10);
-    
-    assert(stack.peek() == 10);
+  stack.push(10);
+
+  assert(stack.peek() == 10);
 }
 
 int main() {
-    test_stack_push();
-    test_stack_pop();
-    test_stack_peek();
-    test_stack_is_empty();
+  test_stack_push();
+  test_stack_pop();
+  test_stack_peek();
+  test_stack_is_empty();
 
-    std::cout << "All tests passed!" << std::endl;
-    return 0;
-    return 0;
+  std::cout << "All tests passed!" << std::endl;
+  return 0;
+  return 0;
 }
