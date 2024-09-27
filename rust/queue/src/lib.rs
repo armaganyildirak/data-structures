2cd7716be0b6ac57cca5c1e40b7b93bccee96155
armaganyildirak <armaganyildirak@gmail.com>
2024-09-26 22:51:15 -0400

    Add tree

--- rust/queue/src/lib.rs
+++ rust/queue/src/lib.rs
@@ -1,20 +1,23 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 struct QueueNode<T> {
     value: T,
-    next: Option<Box<QueueNode<T>>>,
+    next: Option<Rc<RefCell<QueueNode<T>>>>,
 }
 
 pub struct Queue<T> {
-    head: Option<Box<QueueNode<T>>>,
-    tail: Option<*mut QueueNode<T>>,
+    head: Option<Rc<RefCell<QueueNode<T>>>>,
+    tail: Option<Rc<RefCell<QueueNode<T>>>>,
 }
 
-impl<T> Default for Queue<T> {
+impl<T: Clone> Default for Queue<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> Queue<T> {
+impl<T: Clone> Queue<T> {
     pub fn new() -> Self {
         Queue {
             head: None,
@@ -23,25 +26,25 @@ impl<T> Queue<T> {
     }
 
     pub fn enqueue(&mut self, value: T) {
-        let mut new_node = Box::new(QueueNode { value, next: None });
-        let raw_node = &mut *new_node as *mut QueueNode<T>;
+        let new_node = Rc::new(RefCell::new(QueueNode { value, next: None }));
 
-        match self.tail {
-            Some(tail) => unsafe {
-                (*tail).next = Some(new_node);
-            },
-            None => self.head = Some(new_node),
+        if let Some(tail) = self.tail.take() {
+            tail.borrow_mut().next = Some(new_node.clone());
+        } else {
+            self.head = Some(new_node.clone());
         }
-        self.tail = Some(raw_node);
+
+        self.tail = Some(new_node);
     }
 
     pub fn dequeue(&mut self) -> Option<T> {
-        self.head.take().map(|mut head| {
-            self.head = head.next.take();
+        self.head.take().map(|head| {
+            let head_borrowed = head.borrow();
+            self.head = head_borrowed.next.clone();
             if self.head.is_none() {
                 self.tail = None;
             }
-            head.value
+            head_borrowed.value.clone()
         })
     }
 }
